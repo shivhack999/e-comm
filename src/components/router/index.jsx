567b0909da0fcef20c180c1453958176038001f3
shiv <shivhack999@gmail.com>
2024-08-24 09:12:37 +0530

    Your commit message

--- src/components/router/index.jsx
+++ src/components/router/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../../pages/home/Home';
 import Auth from '../../pages/auth/Auth';
-import Employee from '../../admin/Index';
 const index = () => {
   return (
     <React.Fragment>
@@ -11,7 +10,7 @@ const index = () => {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/user" element={<Auth />} />
-            <Route path="/employee" element={<Employee/>} />
+            {/* <Route path="/employee" element={<Employee/>} /> */}
         </Routes>
       </Router>
     </React.Fragment>
