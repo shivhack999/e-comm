a84cef0e78ff7832e506c998881d18c1c5581fb2
shiv <shivhack999@gmail.com>
2024-09-26 22:11:15 +0530

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import './App.css';
-<<<<<<< HEAD:src/App.js
-import Navbar from './components/navbar/Index';
-import Footer from './components/Footer/Index';
-import Router from './components/router/index';
-=======
 import Router from './components/router/Router';
->>>>>>> prince:src/App.jsx
 function App() {
   return (
     <>
